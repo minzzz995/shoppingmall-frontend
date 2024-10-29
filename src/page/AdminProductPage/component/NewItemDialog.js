cac3abf432a63d389b89f66cdda00f8ffaf1e977
minzzz995 <minzzz995@gmail.com>
2024-10-29 10:36:53 +0900

    redirects&admin

--- src/page/AdminProductPage/component/NewItemDialog.js
+++ src/page/AdminProductPage/component/NewItemDialog.js
@@ -9,6 +9,7 @@ import {
   createProduct,
   editProduct,
 } from "../../../features/product/productSlice";
+import { setFromContext } from "@cloudinary/url-gen/actions/variable";
 
 const InitialFormData = {
   name: "",
@@ -31,6 +32,8 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
   const [stock, setStock] = useState([]);
   const dispatch = useDispatch();
   const [stockError, setStockError] = useState(false);
+
+  console.log("stock", stock);
 
   useEffect(() => {
     if (success) setShowDialog(false);
@@ -63,8 +66,18 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    // console.log("formdata", formData)
+    // console.log("formdata", stock);
     //재고를 입력했는지 확인, 아니면 에러
+    if(stock.length === 0) return setStockError(true);
+
     // 재고를 배열에서 객체로 바꿔주기
+    const totalStock = stock.reduce((total, item)=>{
+      return {...total, [item[0]]:parseInt(item[1])}
+    },{})
+
+    console.log("formDta", totalStock);
+
     // [['M',2]] 에서 {M:2}로
     if (mode === "new") {
       //새 상품 만들기
@@ -75,25 +88,37 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
 
   const handleChange = (event) => {
     //form에 데이터 넣어주기
+    const { id, value } = event.target;
+    setFormData({ ...formData, [id]: value });
   };
 
   const addStock = () => {
     //재고타입 추가시 배열에 새 배열 추가
+    setStock([...stock, []]);
   };
 
   const deleteStock = (idx) => {
     //재고 삭제하기
+    const newStock = stock.filter((item, index) => index !== idx);
+    setStock(newStock)
   };
 
   const handleSizeChange = (value, index) => {
     //  재고 사이즈 변환하기
+    const newStock = [...stock]
+    newStock[index][0] = value
+    setStock(newStock)
   };
 
   const handleStockChange = (value, index) => {
     //재고 수량 변환하기
+    const newStock = [...stock]
+    newStock[index][1] = value
+    setStock(newStock)
   };
 
   const onHandleCategory = (event) => {
+    // 카테고리가 이미 추가되어 있으면 제거
     if (formData.category.includes(event.target.value)) {
       const newCategory = formData.category.filter(
         (item) => item !== event.target.value
@@ -103,6 +128,7 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
         category: [...newCategory],
       });
     } else {
+      // 아니면 새로 추가
       setFormData({
         ...formData,
         category: [...formData.category, event.target.value],
@@ -112,6 +138,7 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
 
   const uploadImage = (url) => {
     //이미지 업로드
+    setFormData({...formData, image: url})
   };
 
   return (
@@ -236,7 +263,7 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
             src={formData.image}
             className="upload-image mt-2"
             alt="uploadedimage"
-          ></img>
+          />
         </Form.Group>
 
         <Row className="mb-3">
